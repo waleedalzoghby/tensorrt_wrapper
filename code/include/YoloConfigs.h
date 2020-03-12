ba7621f2234b825507874af5f580cc1b13f0e4f5
waleed <waleed.alzoghby@outlook.com>
2020-03-12 16:58:05 +0100

    kernel modification

--- code/include/YoloConfigs.h
+++ code/include/YoloConfigs.h
@@ -15,22 +15,39 @@ namespace Yolo
         float anchors[CHECK_COUNT*2];
     };
 
-    //YOLO 608
+    //YOLO 800
     YoloKernel yolo1 = {
-        19,
-        19,
+        25,
+        25,
         {116,90,  156,198,  373,326}
     };
     YoloKernel yolo2 = {
-        38,
-        38,
+        50,
+        50,
         {30,61,  62,45,  59,119}
     };
     YoloKernel yolo3 = {
-        76,
-        76,
+        100,
+        100,
         {10,13,  16,30,  33,23}
     };
+
+    //YOLO 608
+    // YoloKernel yolo1 = {
+    //     19,
+    //     19,
+    //     {116,90,  156,198,  373,326}
+    // };
+    // YoloKernel yolo2 = {
+    //     38,
+    //     38,
+    //     {30,61,  62,45,  59,119}
+    // };
+    // YoloKernel yolo3 = {
+    //     76,
+    //     76,
+    //     {10,13,  16,30,  33,23}
+    // };
 
     //YOLO 416
     // YoloKernel yolo1 = {
